a48dcaf52084110474e501637c723b35a9387300
Ahmad AS <a.schigdar@gmail.com>
2024-11-10 16:06:15 +0100

    extended mongodb ingestion

---
+++ app/ingestion/mongodb_ingestion.py
@@ -0,0 +1,32 @@
+import os
+from pymongo import MongoClient
+from app.utils.parsers import parse_pdfs_basic
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+# client = MongoClient("mongodb://root:example@localhost:27017/")
+
+# db = client["mydatabase"]
+# collection = db["mycollection"]
+
+# data = {"name": "Alice", "age": 30, "city": "New York"}
+# collection.insert_one(data)
+
+# result = collection.find_one({"name": "Alice"})
+# print(result)
