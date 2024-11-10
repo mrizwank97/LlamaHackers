668008fe18ffdedf4e010d51bdf68f533a134f7b
Ahmad AS <a.schigdar@gmail.com>
2024-11-10 19:24:15 +0100

    completed mongodb ingestion and cleanup

--- app/ingestion/mongodb_ingestion.py
+++ app/ingestion/mongodb_ingestion.py
@@ -1,32 +1,23 @@
-import os
 from pymongo import MongoClient
 from app.utils.parsers import parse_pdfs_basic
 
 
+
 def main():
-    pass
+    parsed_templates = parse_pdfs_basic(directory="./data/template/")
+    mongodb_ingestion(parsed_templates, "template")
+
+
+def mongodb_ingestion(data, col_name):
+    client = MongoClient("mongodb://root:example@localhost:27017/")
+    db = client["llamahackers"]
+    collection = db[col_name]
+
+    for file_name, parsed_document in data.items():
+        document = {"file_name": file_name, "parsed_document": parsed_document}
+        collection.insert_one(document)
+        print(f"Data inserted for {file_name}.")
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-# client = MongoClient("mongodb://root:example@localhost:27017/")
-
-# db = client["mydatabase"]
-# collection = db["mycollection"]
-
-# data = {"name": "Alice", "age": 30, "city": "New York"}
-# collection.insert_one(data)
-
-# result = collection.find_one({"name": "Alice"})
-# print(result)
